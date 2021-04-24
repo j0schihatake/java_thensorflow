7ed300779d91f63b38bcfe9bc432c7b1e98cde82
j0schi <j0schitadamori@gmail.com>
2021-04-24 13:18:32 +0300

    add project to git

---
+++ src/main/java/HelloTensorFlow.java
@@ -0,0 +1,10 @@
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+
+public class HelloTensorFlow {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
